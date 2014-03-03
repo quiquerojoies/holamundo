edee061d52d097ffac7ef39310b716c188f761ca
quiquerojo <quiquerojoies@gmail.com>
2014-03-03 12:02:42 +0100

    Commit inicial del proyecto Hola Mundo

---
+++ Hola mundo/src/HolaMundo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**Clase HolaMundo para explicar cómo se crea un proyecto en eclipse
+ * a los alumnos de DM1A del curso 2013-14
+ * @author quiquerojo
+ *
+ */
+public class HolaMundo {
+
+	/**método que imprime una frase creativa
+	 * @param args parámetros para el main
+	 */
+	public static void main(String[] args) {
+		//este es un comentario hasta el final de la línea
+		/*
+		 * este es un 
+		 * comentario
+		 * de varias 
+		 * líneas
+		 */
+		System.out.println("Hola queridos amiguitos de primero de DAM");
+
+	}
+
+}
